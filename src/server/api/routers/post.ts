cd451052dd23a21f35fea69a73a9df2ea5b6b830
Jesse Zenon <jz.the.dev@gmail.com>
2024-01-22 17:05:20 -0600

    getting db setup

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -12,20 +12,29 @@ export const postRouter = createTRPCRouter({
     }),
 
   create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
+    .input(
+      z.object({
+        title: z.string().min(1),
+        artist: z.string().min(1),
+        key: z.string().min(1),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      return ctx.db.post.create({
+      return ctx.db.song.create({
         data: {
-          name: input.name,
+          title: input.title,
+          artist: input.artist,
+          key: input.key,
+          createdAt: new Date(),
         },
       });
     }),
 
   getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
+    return ctx.db.song.findFirst({
       orderBy: { createdAt: "desc" },
     });
   }),
