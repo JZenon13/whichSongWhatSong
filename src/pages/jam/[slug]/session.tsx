a81f6897bd1ab057d861267a0702275369a1bba3
Jesse Zenon <jz.the.dev@gmail.com>
2024-02-09 15:05:56 -0600

    refactored session page

--- src/pages/jam/[slug]/session.tsx
+++ src/pages/jam/[slug]/session.tsx
@@ -2,89 +2,66 @@ import Head from "next/head";
 import React, { useState, useEffect } from "react";
 
 export default function Session() {
-  const [currentSong, setCurrentSong] = useState<{
-    song: {
-      id: number;
-      title: string;
-      artist: string;
-      key: string;
-      genre: string;
-      createdAt: Date;
-      updatedAt: Date;
-    } | null;
-  } | null>(null);
-
-  const [nextSong, setNextSong] = useState<{
-    song: {
-      id: number;
-      title: string;
-      artist: string;
-      key: string;
-      genre: string;
-      createdAt: Date;
-      updatedAt: Date;
-    } | null;
-  } | null>(null);
-  const [selectedSongs, setSelectedSongs] = useState<
-    {
-      id: number;
-      title: string;
-      artist: string;
-      key: string;
-      genre: string;
-      createdAt: Date;
-      updatedAt: Date;
-    }[]
-  >([]);
+  type Song = {
+    title: string;
+    artist: string;
+  };
+  interface StoredSong {
+    song: Song;
+  }
+  const [currentSong, setCurrentSong] = useState<Song | null>(null);
+  const [nextSong, setNextSong] = useState<Song | null>(null);
+  const [selectedSongs, setSelectedSongs] = useState<Song[]>([]);
   const TIMER_SETTINGS = {
     SLOW: 60000, // 1 minute
     NORMAL: 45000, // 45 seconds
     FAST: 15000, // 15 seconds
   };
   const [timerSetting, setTimerSetting] = useState(TIMER_SETTINGS.NORMAL);
+  const [sessionComplete, setSessionComplete] = useState(false);
+
   useEffect(() => {
     const storedSongs = localStorage.getItem("displayedSongs");
+
     if (storedSongs) {
-      const songs = JSON.parse(storedSongs) as {
-        id: number;
-        title: string;
-        artist: string;
-        key: string;
-        genre: string;
-        createdAt: Date;
-        updatedAt: Date;
-      }[];
-      setSelectedSongs(songs);
-      if (songs.length > 0) {
-        setCurrentSong({ song: songs[0] ?? null });
-        setNextSong(songs[1] ? { song: songs[1] } : null);
+      const parsedData = JSON.parse(storedSongs) as StoredSong[];
+
+      if (parsedData) {
+        const songs = parsedData.map((item: StoredSong) => item.song);
+        setSelectedSongs(songs);
+        if (songs.length > 1) {
+          setCurrentSong(songs[0] ? songs[0] : null);
+          setNextSong(songs[1] ? songs[1] : null);
+        }
       }
     }
   }, []);
-  const getNextSong = () => {
-    const remainingSongs: {
-      id: number;
-      title: string;
-      artist: string;
-      key: string;
-      genre: string;
-      createdAt: Date;
-      updatedAt: Date;
-    }[] = selectedSongs.filter((song) => song.id !== currentSong?.song?.id);
 
-    if (remainingSongs.length > 0) {
-      const randomIndex = Math.floor(Math.random() * remainingSongs.length);
-      const nextSong = remainingSongs[randomIndex];
-      setCurrentSong({ song: nextSong ?? null });
+  let currentSongIndex = 0;
+
+  const getNextSong = () => {
+    if (selectedSongs.length > 0) {
+      setCurrentSong(selectedSongs[currentSongIndex] ?? null);
+      currentSongIndex = (currentSongIndex + 1) % selectedSongs.length;
+      setNextSong(selectedSongs[currentSongIndex] ?? null);
     } else {
-      setCurrentSong({ song: null });
-      setNextSong({ song: null });
+      setCurrentSong(null);
+      setNextSong(null);
     }
   };
   useEffect(() => {
-    const timer = setInterval(getNextSong, timerSetting);
-    return () => clearInterval(timer);
-  }, [currentSong, nextSong, selectedSongs, timerSetting]);
+    let timer: NodeJS.Timeout | null = null;
+
+    if (!sessionComplete) {
+      timer = setInterval(getNextSong, timerSetting);
+    }
+
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+    };
+  }, [currentSong, nextSong, selectedSongs, timerSetting, sessionComplete]);
 
   return (
     <>
@@ -98,28 +75,57 @@ export default function Session() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div title="SpeedButtons">
-          <button onClick={() => setTimerSetting(TIMER_SETTINGS.SLOW)}>
+          <button
+            className={
+              timerSetting === TIMER_SETTINGS.SLOW
+                ? "ml-4 mr-4 text-red-500"
+                : "ml-4 mr-4 "
+            }
+            onClick={() => setTimerSetting(TIMER_SETTINGS.SLOW)}
+          >
             Slow
           </button>
-          <button onClick={() => setTimerSetting(TIMER_SETTINGS.NORMAL)}>
+          <button
+            className={
+              timerSetting === TIMER_SETTINGS.NORMAL
+                ? "ml-4 mr-4 text-red-500"
+                : "ml-4 mr-4 "
+            }
+            onClick={() => setTimerSetting(TIMER_SETTINGS.NORMAL)}
+          >
             Normal
           </button>
-          <button onClick={() => setTimerSetting(TIMER_SETTINGS.FAST)}>
+          <button
+            className={
+              timerSetting === TIMER_SETTINGS.FAST
+                ? "ml-4 mr-4 text-red-500"
+                : "ml-4 mr-4 "
+            }
+            onClick={() => setTimerSetting(TIMER_SETTINGS.FAST)}
+          >
             Fast
           </button>
         </div>
         <div>Session</div>
-        {currentSong && (
+        {sessionComplete && <div>Session Complete</div>}
+        {!sessionComplete && currentSong && (
           <div>
-            Now Playing: {currentSong.song?.title} by {currentSong.song?.artist}
+            Now Playing: {currentSong.title} by {currentSong.artist}
           </div>
         )}
-        {nextSong && (
+        {!sessionComplete && nextSong && (
           <div>
-            Up Next: {nextSong.song?.title} by {nextSong.song?.artist}
+            Up Next: {nextSong.title} by {nextSong.artist}
           </div>
         )}
-        <button onClick={() => getNextSong()}>Next Song</button>
+        {!sessionComplete && (
+          <button onClick={() => getNextSong()}>Next Song</button>
+        )}
+        {!sessionComplete && (
+          <button onClick={() => setSessionComplete(true)}>
+            Complete Session
+          </button>
+        )}
       </main>
     </>
   );
